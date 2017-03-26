37222eb9442d527859250a656640d5454b4e7202
Francis Tseng <f+accounts@frnsys.com>
2017-03-25 22:00:21 -0400

    updated setup requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='mdpy',
-    version='0.0.3',
+    version='0.0.4',
     description='utilities for converting markdown files to ipython notebooks',
     url='https://github.com/frnsys/mdpy',
     author='Francis Tseng (@frnsys)',
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'click'
+        'click',
+        'nbformat',
+        'nbconvert'
     ],
     entry_points='''
         [console_scripts]
